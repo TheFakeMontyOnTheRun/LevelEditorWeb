4242f615ce0e489c2a7fbe8ccdd1d71e9cce8966
Daniel Monteiro <danielmonteiro@id.uff.br>
2020-03-15 19:44:00 +0000

    Add WASM loading

--- src/main/html/scripts.js
+++ src/main/html/scripts.js
@@ -42,6 +42,50 @@ function httpPost(theUrl, data) {
     return xmlHttp.responseText;
 }
 
+function initWASM() {
+    // Check for wasm support.
+    if (!('WebAssembly' in window)) {
+	alert('you need a browser with wasm support enabled :(');
+    }
+    
+    // Loads a WebAssembly dynamic library, returns a promise.
+    // imports is an optional imports object
+    function loadWebAssembly(filename, imports) {
+	// Fetch the file and compile it
+	return fetch(filename)
+	    .then(response => response.arrayBuffer())
+	    .then(buffer => WebAssembly.compile(buffer))
+	    .then(module => {
+		    // Create the imports for the module, including the
+		    // standard dynamic library imports
+		    imports = imports || {};
+		    imports.env = imports.env || {};
+		    imports.env.memoryBase = imports.env.memoryBase || 0;
+		    imports.env.tableBase = imports.env.tableBase || 0;
+		    if (!imports.env.memory) {
+			imports.env.memory = new WebAssembly.Memory({ initial: 256 });
+		    }
+		    if (!imports.env.table) {
+			imports.env.table = new WebAssembly.Table({ initial: 0, element: 'anyfunc' });
+		    }
+		    // Create the instance.
+		    return new WebAssembly.Instance(module, imports);
+		});
+    }
+    
+    // Main part of this example, loads the module and uses it.
+    loadWebAssembly('demo.wasm')
+	.then(instance => {
+		var exports = instance.exports; // the exports of that instance
+		var func = exports.func;
+		
+		
+		alert("func is " + func());
+	    }
+	    );
+    
+}
+
 function init() {
     var x = 0;
     var y = 0;
@@ -51,6 +95,7 @@ function init() {
 	}
     }
     initPalette();
+    initWASM();
 }
 
 function draw() {
